f6e03b2bec0da4574847633a713d851a59c4f0d0
Pascal <pasalberti@gmail.com>
2024-01-19 17:16:06 +0100

    Initial Commit

---
+++ Stats MK/Stats_MKApp.swift
@@ -0,0 +1,32 @@
+//
+//  Stats_MKApp.swift
+//  Stats MK
+//
+//  Created by Pascal Alberti on 19/01/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Stats_MKApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Stats MKUITests/Stats_MKUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Stats_MKUITestsLaunchTests.swift
+//  Stats MKUITests
+//
+//  Created by Pascal Alberti on 19/01/2024.
+//
+
+import XCTest
+
+final class Stats_MKUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
